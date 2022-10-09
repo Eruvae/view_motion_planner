bf33283f301cea0373b38ffe1fbc28903fc8d8e9
rohit.menon <rohit.menon@uni-bonn.de>
2022-10-10 01:43:16 +0200

    get pose limit from rosparam

--- src/view_motion_planner.cpp
+++ src/view_motion_planner.cpp
@@ -9,7 +9,8 @@ ViewMotionPlanner::ViewMotionPlanner(ros::NodeHandle &nh, tf2_ros::Buffer &tfBuf
                                      const std::string &map_frame, const std::string &ws_frame, const std::string &robot_description_param_name,
                                      const std::string &group_name, const std::string &ee_link_name, double tree_resolution, size_t graph_builder_threads,
                                      bool update_planning_tree, bool evaluation_mode, size_t eval_num_episodes, EvalEpisodeEndParam ep, double eval_episode_duration)
-  : random_engine(std::random_device{}()),
+  :nh_(nh), 
+  random_engine(std::random_device{}()),
     NUM_GRAPH_BUILDER_THREADS(graph_builder_threads),
     update_planning_tree(update_planning_tree),
     evaluation_mode(evaluation_mode),
@@ -478,7 +479,8 @@ void ViewMotionPlanner::resumeGraphBuilderThreads()
 
 void ViewMotionPlanner::exploreNamedPoses()
 {
-  const std::string pose_list[] = {"explpose1", "explpose2", "explpose3", "explpose4", "explpose5", "home"};
+  std::vector<std::string> pose_list;
+  nh_.param<std::vector<std::string>>("view_motion_planner/pose_list", pose_list, {"explpose1", "explpose2", "explpose3", "explpose4", "explpose5", "home"});
 
   for (const std::string &pose : pose_list)
   {
